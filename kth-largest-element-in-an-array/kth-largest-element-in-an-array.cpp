8543fb06182b62d42c195187093038a9d7f870a9
Arspasov <72569770+Arspasov@users.noreply.github.com>
2021-01-29 12:15:15 +0200

    Time: 12 ms (57.40%), Space: 10 MB (69.54%) - LeetHub

---
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        auto kth = nums.begin() + (nums.size()  - k);
+           std::nth_element(nums.begin(),nums.begin() + (nums.size() - k) , nums.end());
+        return *kth;
+    }
+};
+
+//brute force: to sort array and then find the largest from right to left;
+// its not useful because we do a lot of useless operations for sorting the array;
+
+//can be done with heap because we can create heap with linear complexity and we can access the top element easily
