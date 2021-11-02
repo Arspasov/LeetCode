b329ece232bcb2eb75f9b69554bb713d2c6757a5
Arspasov <72569770+Arspasov@users.noreply.github.com>
2021-11-02 13:04:15 +0200

    Time: 4 ms (98.73%), Space: 11.6 MB (77.59%) - LeetHub

--- remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
+++ remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
@@ -1,42 +1,38 @@
 /**
  * Definition for singly-linked list.
  * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
 public:
-    ListNode* deleteDuplicates(ListNode* head) {
-        if(!head){
-            return head;
-        }
-        ListNode* newHead = new ListNode(head-> val);
-        ListNode* tail = newHead;
-        //1_a->1_b->2
-        //1->2
-        while(head){
-            //nullptr
-            if(head->val != tail ->val){
-                tail->next = new ListNode(head -> val);
-                tail = tail ->next;
-            }
-            head = head->next;
-        }
-        
-        return newHead;
-    }
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(!head){
+            return head;
+        }
+        
+        ListNode* current = head;
+        while(current->next !=NULL){
+            if(current -> next->val == current->val){
+                current->next = current->next->next;
+            }else{
+                current = current->next;
+            }
+        }
+        return head;
+    }
 };
-​
+
 /*
 if head == nullptr // size = 0;
-​
+
 if size == 1;
 return head;
-​
+
 1->1->2
 1,1,2
 1->2
