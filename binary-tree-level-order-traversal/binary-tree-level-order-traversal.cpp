92d5901057dd7420af750eb89d94a3688ff881ac
Arspasov <72569770+Arspasov@users.noreply.github.com>
2021-11-15 15:55:44 +0200

    Time: 8 ms (35.48%), Space: 13.4 MB (11.75%) - LeetHub

--- binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -1,38 +1,40 @@
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-    queue <TreeNode*> frontier;
-        frontier.push(root);
-        queue<TreeNode*> next_frontier;
-        
-        vector<vector<int>> levels;
-        levels.push_back(vector<int>());
-        while(!frontier.empty()){
-            TreeNode* current = frontier.front();
-            frontier.pop();
-            if(current){
-                levels[levels.size() - 1].push_back(current -> val); 
-                next_frontier.push(current-> left);
-                next_frontier.push(current -> right);
-                }
-            if(frontier.empty()){
-                frontier = queue<TreeNode*>(move(next_frontier));
-                levels.push_back(vector<int>());
-            }
-            }
-        levels.pop_back();
-        levels.pop_back();
-    return levels;
-    }
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        queue<TreeNode*> frontier;
+        frontier.push(root);
+        queue<TreeNode*> next_frontier;
+        
+        vector<vector<int>> levels;
+        levels.push_back(vector<int>());
+        while(!frontier.empty()){
+            TreeNode* current = frontier.front();
+            frontier.pop();
+            if(current){
+                levels[levels.size() -1].push_back(current->val);
+                cout<< current -> val << ' ';
+                next_frontier.push(current->left);
+                next_frontier.push(current->right);
+            }
+            
+            if(frontier.empty()){
+                frontier = queue<TreeNode*>(move(next_frontier));
+                levels.push_back(vector<int> ());
+            }
+        }
+        levels.pop_back();
+        levels.pop_back();
+        return  levels;
+    }
 };
